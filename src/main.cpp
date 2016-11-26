c5b3486f769da999e7bf7df5421ff0f2f3a254dc
robin <robin.kirkman@gmail.com>
2016-11-26 13:44:09 -0500

    some prettier example stuff

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,32 @@
 
 #include <t3_Adafruit_SSD1306.h>
 
+#include <Fonts/FreeMonoBold9pt7b.h>
+
 t3_Adafruit_SSD1306 gfx0(&Wire);
 t3_Adafruit_SSD1306 gfx1(&Wire1);
 
 void setup() {
 	gfx0.begin();
 	gfx0.clearDisplay();
-	gfx0.setTextColor(WHITE);
-	gfx0.print("gfx0");
+	gfx0.fillRect(0, 0, 128, 9, WHITE);
+	gfx0.setTextColor(BLACK);
+	gfx0.setCursor(4, 1);
+	gfx0.print("SISC VM OLED CONSOLE");
 	gfx0.display();
 
 	gfx1.begin();
 	gfx1.clearDisplay();
 	gfx1.setTextColor(WHITE);
-	gfx1.print("gfx1");
+	gfx1.setFont(&FreeMonoBold9pt7b);
+	gfx1.setCursor(0, 15);
+	gfx1.print("PC: 0x0000");
+	gfx1.setCursor(0, 31);
+	gfx1.print(" A: 0x0000");
+	gfx1.setCursor(0, 47);
+	gfx1.print(" B: 0x0000");
+	gfx1.setCursor(0, 63);
+	gfx1.print(" C: 0x0000");
 	gfx1.display();
 }
 
