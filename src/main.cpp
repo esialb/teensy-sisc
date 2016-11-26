52d053dc280236289944276a5fc47a9dc5b9d53f
robin <robin.kirkman@gmail.com>
2016-11-25 22:25:16 -0500

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,20 @@
 
 
 #include <Arduino.h>
+#include <i2c_t3.h>
+#include <stdint.h>
+
+#include "Adafruit_SSD1306.h"
+
+Adafruit_SSD1306 gfx0(&Wire);
+Adafruit_SSD1306 gfx1(&Wire1);
 
 void setup() {
+	gfx0.begin();
+	gfx0.display();
 
+	gfx1.begin();
+	gfx1.display();
 }
 
 void loop() {
